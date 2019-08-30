32c8c31a75e4b3ccf37e36134ddd45d949d31bee
Randy Spurlock <randy@spurlock.us>
2019-08-30 00:40:15 -0500

    Modify comments and spacing to conform to Python standard

--- students/rspurlock/lesson1/break_me.py
+++ students/rspurlock/lesson1/break_me.py
@@ -1,28 +1,32 @@
-#Function to generate a NameError by trying to print a missing variable
+# Function to generate a NameError by trying to print a missing variable
 def nameError():
     print(missingVariable)
 
-#Function to generate a TypeError by trying to divide a number by a string
+
+# Function to generate a TypeError by trying to divide a number by a string
 def typeError():
     print(1/"string")
 
-#Function to generate a SyntaxError by trying to evaluate invalid python code
+
+# Function to generate a SyntaxError by trying to evaluate invalid python code
 def syntaxError():
     eval("1/%0")
 
-#Function to generate a AttributeError by trying to use an invalid attribute from system object
+
+# Function to generate a AttributeError by trying to use an invalid attribute from system object
 def attributeError():
     import sys
     sys.bogus
 
-#Uncomment the following line to generate a NameError
+
+# Uncomment the following line to generate a NameError
 #nameError()
 
-#Uncomment the following line to generate a TypeError
+# Uncomment the following line to generate a TypeError
 #typeError()
 
-#Uncomment the following line to generate a SyntaxError
+# Uncomment the following line to generate a SyntaxError
 #syntaxError()
 
-#Uncomment the following line to generate a AttributeError
+# Uncomment the following line to generate a AttributeError
 #attributeError()
