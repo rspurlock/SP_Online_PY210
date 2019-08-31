73bb09a4393a228282a03fac70003015a604bbb8
Randy Spurlock <randy@spurlock.us>
2019-08-30 22:15:33 -0500

    File for CodingBat Warmup_2 exercises

---
+++ students/rspurlock/lesson1/Warmup_2.py
@@ -0,0 +1,158 @@
+# Function to return the given string duplicate the given number of times
+def string_times(str, n):
+    result = ''
+    for x in range(n):
+        result += str
+    return result
+
+
+# Test cases for string_times function
+print('string_times')
+print("string_times('Hi', 2) → ", string_times('Hi', 2))
+print("string_times('Hi', 3) → ", string_times('Hi', 3))
+print("string_times('Hi', 1) → ", string_times('Hi', 1))
+print()
+
+
+# Function to return the front 3 characters of the given string the requested number of times
+#   If the string is less than 3 characters then the string is the front
+def front_times(str, n):
+    result = ''
+    if (len(str) < 4):
+        for x in range(n):
+            result += str
+    else:
+        for x in range(n):
+            result += str[0:3]
+    return result
+
+
+# Test cases for front_times function
+print('front_times')
+print("front_times('Chocolate', 2) → ", front_times('Chocolate', 2))
+print("front_times('Chocolate', 3) → ", front_times('Chocolate', 3))
+print("front_times('Abc', 3) → ", front_times('Abc', 3))
+print()
+
+
+# Function to return a new string made up of every other character of the given string
+def string_bits(str):
+    pos = 0
+    result = ''
+    for x in range((len(str) + 1) // 2):
+        result += str[pos:pos + 1]
+        pos += 2
+    return result
+
+
+# Test cases for string_bits function
+print('string_bits')
+print("string_bits('Hello') → ", string_bits('Hello'))
+print("string_bits('Hi') → ", string_bits('Hi'))
+print("string_bits('Heeololeo') → ", string_bits('Heeololeo'))
+print()
+
+
+# Function to return a string "splosion" a new string made up of a combination of each substring of the given string
+def string_splosion(str):
+    result = ''
+    for x in range(len(str)):
+        result += str[0:x + 1]
+    return result
+
+
+# Test cases for string_splosion function
+print('string_splosion')
+print("string_splosion('Code') → ", string_splosion('Code'))
+print("string_splosion('abc') → ", string_splosion('abc'))
+print("string_splosion('ab') → ", string_splosion('ab'))
+print()
+
+
+# Function to return a count of the number of times a substring of the last 2 characters appears in the given string
+def last2(str):
+    count = 0
+    if (len(str[-2:]) == 2):
+        for x in range(len(str) - 2):
+            if (str[x:x + 2] == str[-2:]):
+                count += 1
+    return count
+
+
+# Test cases for last2 function
+print('last2')
+print("last2('hixxhi') → ", last2('hixxhi'))
+print("last2('xaxxaxaxx') → ", last2('xaxxaxaxx'))
+print("last2('axxxaaxx') → ", last2('axxxaaxx'))
+print()
+
+
+# Function to count the number of 9's in an array
+def array_count9(nums):
+    count = 0
+    for x in nums:
+        if (x == 9):
+            count += 1
+    return count
+
+
+# Test cases for array_count9 function
+print('array_count9')
+print("array_count9([1, 2, 9]) → ", array_count9([1, 2, 9]))
+print("array_count9([1, 9, 9]) → ", array_count9([1, 9, 9]))
+print("array_count9([1, 9, 9, 3, 9]) → ", array_count9([1, 9, 9, 3, 9]))
+print()
+
+
+# Function to return True if one of the first four elements of the array is 9
+#   The array length may be less than four
+def array_front9(nums):
+    count = min(4, len(nums))
+    result = False
+    for x in range(count):
+        if (nums[x] == 9):
+            result = True
+    return result
+
+
+# Test cases for array_front9 function
+print('array_front9')
+print("array_front9([1, 2, 9, 3, 4]) → ", array_front9([1, 2, 9, 3, 4]))
+print("array_front9([1, 2, 3, 4, 9]) → ", array_front9([1, 2, 3, 4, 9]))
+print("array_front9([1, 2, 3, 4, 5]) → ", array_front9([1, 2, 3, 4, 5]))
+print()
+
+
+# Function to return True if the sequence 1, 2, 3 appears in the given array
+def array123(nums):
+    result = False
+    for x in range(len(nums) - 2):
+        if ((nums[x] == 1) and (nums[x + 1] == 2) and (nums[x + 2] == 3)):
+            result = True
+    return result
+
+
+# Test cases for array123 function
+print('array123')
+print("array123([1, 1, 2, 3, 1]) → ", array123([1, 1, 2, 3, 1]))
+print("array123([1, 1, 2, 4, 1]) → ", array123([1, 1, 2, 4, 1]))
+print("array123([1, 1, 2, 1, 2, 3]) → ", array123([1, 1, 2, 1, 2, 3]))
+print()
+
+
+# Function to return the number of positions where two strings have the same 2 character substring
+def string_match(a, b):
+    num = min(len(a), len(b)) - 1
+    count = 0
+    for x in range(num):
+        if (a[x:x + 2] == b[x:x + 2]):
+            count += 1
+    return count
+
+
+# Test cases for string_match function
+print('string_match')
+print("string_match('xxcaazz', 'xxbaaz') → ", string_match('xxcaazz', 'xxbaaz'))
+print("string_match('abc', 'abc') → ", string_match('abc', 'abc'))
+print("string_match('abc', 'axc') → ", string_match('abc', 'axc'))
+print()
