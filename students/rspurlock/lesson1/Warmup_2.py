aa82b2036ebd8b61ead276971c12e09d0bef6223
Randy Spurlock <randy@spurlock.us>
2019-09-01 13:32:45 -0500

    Add proper docstrings to Warmup2 functions

--- students/rspurlock/lesson1/Warmup_2.py
+++ students/rspurlock/lesson1/Warmup_2.py
@@ -1,5 +1,12 @@
-# Function to return the given string duplicate the given number of times
 def string_times(str, n):
+    """
+    Function to return the given string duplicated the given number of times
+
+    :param str: The string to duplicate
+    :param n:   The number of times to duplicate the string
+
+    :return:    New string that is the original string duplicated the requested number of times
+    """
     result = ''
     for x in range(n):
         result += str
@@ -14,9 +21,16 @@ print("string_times('Hi', 1) → ", string_times('Hi', 1))
 print()
 
 
-# Function to return the front 3 characters of the given string the requested number of times
-#   If the string is less than 3 characters then the string is the front
 def front_times(str, n):
+    """
+    Function to return the front 3 characters of the given string the requested number of times
+        If the given string is less than 3 characters then the string is the front
+
+    :param str: String to get the front 3 characters from
+    :param n:   The number of times to duplicate the front 3 characters
+
+    :return:    New string that is the front 3 characters duplicated the requested number of times
+    """
     result = ''
     if (len(str) < 4):
         for x in range(n):
@@ -35,8 +49,14 @@ print("front_times('Abc', 3) → ", front_times('Abc', 3))
 print()
 
 
-# Function to return a new string made up of every other character of the given string
 def string_bits(str):
+    """
+    Function to return a new string made up of every other character of the given string
+
+    :param str: String to extract every other character from
+
+    :return:    New string that is made up of every other character from the original string
+    """
     pos = 0
     result = ''
     for x in range((len(str) + 1) // 2):
@@ -53,8 +73,14 @@ print("string_bits('Heeololeo') → ", string_bits('Heeololeo'))
 print()
 
 
-# Function to return a string "splosion" a new string made up of a combination of each substring of the given string
 def string_splosion(str):
+    """
+    Function to retuen a string "splosion", i.e. a new string made up of a combination of each substring of the given string
+
+    :param str: Original string to generate the "splosion" string from
+
+    :return:    New string made up of a combination of each substring of the given string
+    """
     result = ''
     for x in range(len(str)):
         result += str[0:x + 1]
@@ -69,8 +95,16 @@ print("string_splosion('ab') → ", string_splosion('ab'))
 print()
 
 
-# Function to return a count of the number of times a substring of the last 2 characters appears in the given string
 def last2(str):
+    """
+    Function to return a count of the number of times a substring of the last 2 characters appears in the given string
+        Not counting the actual last 2 character substring itself
+
+    :param str: String to count the number of times the substring of the last 2 characters occurs
+
+    :return:    Number of times the substring of the last 2 characters appears in the original string
+                    Not counting the actual last 2 character substring itself
+    """
     count = 0
     if (len(str[-2:]) == 2):
         for x in range(len(str) - 2):
@@ -87,8 +121,14 @@ print("last2('axxxaaxx') → ", last2('axxxaaxx'))
 print()
 
 
-# Function to count the number of 9's in an array
 def array_count9(nums):
+    """
+    Function to count the number of 9's in an array
+
+    :param nums:    Array containing the numbers to check
+
+    :return:        The number of 9's in the given array
+    """
     count = 0
     for x in nums:
         if (x == 9):
@@ -104,9 +144,15 @@ print("array_count9([1, 9, 9, 3, 9]) → ", array_count9([1, 9, 9, 3, 9]))
 print()
 
 
-# Function to return True if one of the first four elements of the array is 9
-#   The array length may be less than four
 def array_front9(nums):
+    """
+    Function to return True if one of the first four elements of the array is 9
+        The array length may be less than four
+
+    :param nums:    Array of numbers to check (the length may be less than four)
+
+    :return:        True if one of the first four elements of the array is 9
+    """
     count = min(4, len(nums))
     result = False
     for x in range(count):
@@ -123,8 +169,14 @@ print("array_front9([1, 2, 3, 4, 5]) → ", array_front9([1, 2, 3, 4, 5]))
 print()
 
 
-# Function to return True if the sequence 1, 2, 3 appears in the given array
 def array123(nums):
+    """
+    Function to return True if the sequence 1, 2, 3 appears in the given array
+
+    :param nums:    Array to check for the sequence 1, 2, 3
+
+    :return:        True is the given array contains the sequence 1, 2, 3
+    """
     result = False
     for x in range(len(nums) - 2):
         if ((nums[x] == 1) and (nums[x + 1] == 2) and (nums[x + 2] == 3)):
@@ -140,8 +192,15 @@ print("array123([1, 1, 2, 1, 2, 3]) → ", array123([1, 1, 2, 1, 2, 3]))
 print()
 
 
-# Function to return the number of positions where two strings have the same 2 character substring
 def string_match(a, b):
+    """
+    Function to return the number of positions where two strings have the same 2 character substring
+
+    :param a:   First string sequence to check
+    :param b:   Second string sequence to check
+
+    :return:    The number of positions where the two strings have the same 2 character substring
+    """
     num = min(len(a), len(b)) - 1
     count = 0
     for x in range(num):
