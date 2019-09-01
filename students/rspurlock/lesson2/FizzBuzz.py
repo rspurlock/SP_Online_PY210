c095fce7f47345eed7396aeec5d2a9a7bdc70d03
Randy Spurlock <randy@spurlock.us>
2019-08-31 20:54:42 -0500

    File for FizzBuzz Exercise

---
+++ students/rspurlock/lesson2/FizzBuzz.py
@@ -0,0 +1,18 @@
+# Function to print the numbers from 1 to 100 inclusive
+#   Except if the number is a multiple of 3 print Fizz instead
+#   Except if the number is a multiple of 5 print Buzz instead
+#   Except if the number is a multiple of both 3 & 5 print FizzBuzz instead
+def fizzBuzz():
+    for x in range(1, 101):
+        if ((x % (3 * 5)) == 0):
+            print('FizzBuzz')
+        elif ((x % 5) == 0):
+            print('Buzz')
+        elif ((x % 3) == 0):
+            print('Fizz')
+        else:
+            print(x)
+
+
+# Test case for fizzBuzz function
+fizzBuzz()
